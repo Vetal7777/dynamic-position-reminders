8f08d69ea4a22cf5fee19d058f2613a611e12993
Kuznetsov Vitalii <jobschildren@gmail.com>
2022-12-10 16:13:41 +0200

    main/fix bugs

--- src/state/initialState.ts
+++ src/state/initialState.ts
@@ -4,31 +4,7 @@ const initialState:InitialState = {
     newItem: null,
     editMode: null,
     createMode: null,
-    list:[
-        {
-            type: 'Category',
-            title: 'First',
-            id: 111111,
-            children: []
-        },
-        {
-            type: 'Category',
-            title: 'Second',
-            id: 22222,
-            children: []
-        },
-        {
-            type: 'Category',
-            title: 'Fourth',
-            id: 444444,
-            children: [{
-                type: 'Category',
-                title: 'Fives',
-                id: 555555,
-                children: []
-            }]
-        },
-    ],
+    list:[],
     size: 1,
     sizeVariables: [0.25,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.25,1.5],
     position: {x:0,y:0}
